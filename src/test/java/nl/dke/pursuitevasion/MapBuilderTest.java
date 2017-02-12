604e103815960e5771d475d99e70a3aab12425f0
nikvaessen <nikvaes@gmail.com>
2017-02-12 19:48:28 +0100

    added a  to test to check if a simple map gets constructed correctly using a MapBuilder

--- src/test/java/nl/dke/pursuitevasion/MapBuilderTest.java
+++ src/test/java/nl/dke/pursuitevasion/MapBuilderTest.java
@@ -1,9 +1,16 @@
 package nl.dke.pursuitevasion;
 
+import nl.dke.pursuitevasion.agents.AbstractAgent;
+import nl.dke.pursuitevasion.gui.simulator.MapViewPanel;
 import nl.dke.pursuitevasion.map.builders.MapBuilder;
+import nl.dke.pursuitevasion.map.impl.Map;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runners.JUnit4;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by nik on 2/9/17.
@@ -13,18 +20,25 @@ public class MapBuilderTest
     @Test
     public void constructAMap()
     {
-        MapBuilder.create(1)
-                .makeFloor()
-                    .addObstacle(new Polygon())
-                    .addObstacle(new Polygon())
-                    .finish(new Polygon())
-                .makeFloor()
-                    .addObstacle(new Polygon())
-                    .finish(new Polygon())
-                .makeFloor()
-                    .finish(new Polygon())
-                //.makeGate()
-                //.verify()
-                .build();
+        Polygon mainFloor = new Polygon(
+                new int[] {   0, 100, 100,   0},
+                new int[] {   0,   0, 100, 100},
+                4
+        );
+
+        Polygon obstacle = new Polygon(
+                new int[] {40, 60, 60, 40},
+                new int[] {40, 40, 60, 60},
+                4
+        );
+
+        Assert.assertNotNull(
+                MapBuilder.create()
+                        .makeFloor(mainFloor)
+                        .addObstacle(obstacle)
+                        .finish()
+                        .build()
+        );
     }
+
 }
