85e11a7528e8b40331b78bdd7b464f1386ac2be8
nikvaessen <nikvaes@gmail.com>
2017-02-12 19:50:16 +0100

    removed unused imports

--- src/test/java/nl/dke/pursuitevasion/MapBuilderTest.java
+++ src/test/java/nl/dke/pursuitevasion/MapBuilderTest.java
@@ -1,16 +1,11 @@
 package nl.dke.pursuitevasion;
 
-import nl.dke.pursuitevasion.agents.AbstractAgent;
-import nl.dke.pursuitevasion.gui.simulator.MapViewPanel;
 import nl.dke.pursuitevasion.map.builders.MapBuilder;
-import nl.dke.pursuitevasion.map.impl.Map;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runners.JUnit4;
 
-import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+
 
 /**
  * Created by nik on 2/9/17.
@@ -21,14 +16,14 @@ public class MapBuilderTest
     public void constructAMap()
     {
         Polygon mainFloor = new Polygon(
-                new int[] {   0, 100, 100,   0},
-                new int[] {   0,   0, 100, 100},
+                new int[]{0, 100, 100, 0},
+                new int[]{0, 0, 100, 100},
                 4
         );
 
         Polygon obstacle = new Polygon(
-                new int[] {40, 60, 60, 40},
-                new int[] {40, 40, 60, 60},
+                new int[]{40, 60, 60, 40},
+                new int[]{40, 40, 60, 60},
                 4
         );
 
