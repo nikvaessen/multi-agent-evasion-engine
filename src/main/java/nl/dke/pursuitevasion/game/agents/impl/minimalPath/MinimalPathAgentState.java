217593258b5b2e18dab1b5d0732ddb2594ccb9bb
Nibbla <Niebisch.markus@gmx.de>
2017-06-25 22:42:06 +0200

    eee3333

---
+++ src/main/java/nl/dke/pursuitevasion/game/agents/impl/minimalPath/MinimalPathAgentState.java
@@ -0,0 +1,12 @@
+package nl.dke.pursuitevasion.game.agents.impl.minimalPath;
+
+/**
+ * Created by Jan on 1-6-2017.
+ */
+public enum MinimalPathAgentState {
+    NO_PATH,
+    MOVING_TO_PATH,
+    MOVING_TO_PROJECTION,
+    ON_PROJECTION,
+    CHASING
+}
---
+++ src/main/java/nl/dke/pursuitevasion/game/agents/impl/minimalPath/MinimalPathOverseerState.java
@@ -0,0 +1,12 @@
+package nl.dke.pursuitevasion.game.agents.impl.minimalPath;
+
+/**
+ * Created by nik on 14/06/17.
+ */
+public enum MinimalPathOverseerState
+{
+    FIRST_AGENT_STABILISING,
+    SECOND_AGENT_STABILISING,
+    THIRD_AGENT_CHASING,
+    THIRD_AGENT_EVICTING
+}
